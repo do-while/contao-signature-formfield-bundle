d9feebf7692a7a56526a2fc05b5c257ce42af601
Hagen Klemp <info@softleister.de>
2023-09-22 20:10:32 +0200

    bgcolor und pen wurden nicht korrekt übernommen

--- src/Resources/contao/forms/FormSignature.php
+++ src/Resources/contao/forms/FormSignature.php
@@ -3,7 +3,7 @@
 /**
  * Extension for Contao 4
  *
- * @copyright  Softleister 2019
+ * @copyright  Softleister 2019-2023
  * @author     Softleister <info@softleister.de>
  * @package    contao-signature-formfield-bundle
  *             based on jSignature (https://github.com/brinley/jSignature)
@@ -11,6 +11,9 @@
  */
 
 namespace Softleister;
+
+use Contao\Widget;
+use Contao\StringUtil;
 
 /**
  * Class FormSignature
@@ -23,75 +26,67 @@ namespace Softleister;
  * @property integer $rows
  * @property integer $cols
  */
-class FormSignature extends \Widget
+class FormSignature extends Widget
 {
-	protected $blnSubmitInput = true;					// Submit user input		@var boolean
-	protected $strTemplate = 'form_signature';			// Template					@var string
-	protected $strPrefix = 'widget widget-signature';	// The CSS class prefix		@var string
+    protected $blnSubmitInput = true;                   // Submit user input        @var boolean
+    protected $strTemplate = 'form_signature';          // Template                 @var string
+    protected $strPrefix = 'widget widget-signature';   // The CSS class prefix     @var string
 
-	/**
-	 * Add specific attributes
-	 *
-	 * @param string $strKey   The attribute name
-	 * @param mixed  $varValue The attribute value
-	 */
-	public function __set( $strKey, $varValue )
-	{
-		switch( $strKey ) {
-			case 'sig_size':		$arrSize = \StringUtil::deserialize($varValue);
-									$this->intWidth = is_numeric($arrSize[0]) ? $arrSize[0] : 500;
-									$this->intHeight = is_numeric($arrSize[1]) ? $arrSize[1] : 250;
-									break;
+    /**
+     * Add specific attributes
+     *
+     * @param string $strKey   The attribute name
+     * @param mixed  $varValue The attribute value
+     */
+    public function __set( $strKey, $varValue )
+    {
+        switch( $strKey ) {
+            case 'sig_size':        $arrSize = StringUtil::deserialize( $varValue );
+                                    $this->intWidth = is_numeric( $arrSize[0] ) ? $arrSize[0] : 500;
+                                    $this->intHeight = is_numeric( $arrSize[1] ) ? $arrSize[1] : 250;
+                                    break;
 
-			case 'sig_bgcolor':		$this->sig_bgcolor = empty($this->sig_bgcolor) ? 'transparent' : '#' . $this->sig_bgcolor;
-									break;
+            case 'sig_bgcolor':     
+                                    $this->sig_bgcolor = empty( $varValue ) ? 'transparent' : '#' . $varValue;
+                                    break;
 
-			case 'sig_width':		$this->sig_width = is_numeric($this->sig_width) && ($this->sig_width > 0) ? $this->sig_width : 5;
-									break;
+            case 'sig_width':       
+                                    $this->sig_width = is_numeric( $varValue ) && ($varValue > 0) ? $varValue : 5;
+                                    break;
 
-			case 'sig_color':		$varValue = $varValue == '' ? '000' : $varValue;
+            case 'sig_color':       $varValue = $varValue == '' ? '#000' : '#' . $varValue;
 
-			default:				parent::__set($strKey, $varValue);
-		}
-	}
+            default:                parent::__set( $strKey, $varValue );
+        }
+    }
 
-	/**
-	 * Return a parameter
-	 *
-	 * @param string $strKey The parameter key
-	 *
-	 * @return mixed The parameter value
-	 */
-	public function __get( $strKey )
-	{
-		switch( $strKey ) {
-			case 'width':			return $this->intWidth;
-									break;
+    /**
+     * Return a parameter
+     *
+     * @param string $strKey The parameter key
+     *
+     * @return mixed The parameter value
+     */
+    public function __get( $strKey )
+    {
+        switch( $strKey ) {
+            case 'width':           return $this->intWidth;
+            case 'height':          return $this->intHeight;
+            case 'pen':             return $this->sig_width;
+            case 'color':           return $this->sig_color;
+            case 'bgcolor':         return $this->sig_bgcolor;
 
-			case 'height':			return $this->intHeight;
-									break;
+            default:                return parent::__get( $strKey );
+        }
+    }
 
-			case 'pen':				return $this->sig_width;
-									break;
-
-			case 'color':			return $this->sig_color;
-									break;
-
-			case 'bgcolor':			return $this->sig_bgcolor;
-									break;
-
-			default:				return parent::__get($strKey);
-									break;
-		}
-	}
-
-	/**
-	 * Generate the widget and return it as string
-	 *
-	 * @return string The widget markup
-	 */
-	public function generate( )
-	{
-		return '';
-	}
+    /**
+     * Generate the widget and return it as string
+     *
+     * @return string The widget markup
+     */
+    public function generate( )
+    {
+        return '';
+    }
 }
